192ed5e905cc2dc7eb559270b778e4daed5ca7e7
Willem Jan Faber <willemjan.faber@kb.nl>
2021-07-26 16:19:40 +0200

    Added base support for multiner.

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -6,20 +8,46 @@ from bert import Ner
 app = Flask(__name__)
 CORS(app)
 
-model = Ner("out_base")
+model = Ner("out_base1")
 
 
-@app.route("/predict/", methods=['GET'])
+#@app.route("/predict/", methods=['GET'])
 def predict():
     text = request.args.get('text')
     if len(text) >= 512:
         return jsonify({"result": 'too large!'})
     # try:
     out = model.predict(text)
+
+
+    text_parsed = ""
+
+    result = []
+    
+    ne = ""
+    for r in out:
+        text_parsed += r.get('word') + ' '
+        if r.get('tag') != 'O':
+            p_tag = r.get('tag')
+            ne += r.get('word') + ' '
+
+            if 'per' in p_tag:
+                lookup_type = 'person'
+            if 'loc' in p_tag:
+                lookup_type = 'location'
+            if 'misc' in p_tag:
+                lookup_type = 'misc'
+        else:
+            if p_tag != 'O':
+                if not ne.strip() in [ne.get('ne') for ne in result]:
+                    pos = text_parsed.find(ne.strip())
+                    result.append({'ne': ne.strip(), 'type': lookup_type, 'pos' : pos})
+                    p_tag = 'O'
+
+    #from pprint import pprint
+    #pprint(result)
+
     return jsonify({"result": out})
-    # except Exception as e:
-    #    print(e)
-    #    return jsonify({"result":"Model Failed"})
 
 
 if __name__ == "__main__":
